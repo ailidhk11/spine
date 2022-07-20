28c80d1d877aeffc1be7ebdff1d882c9c361978a
ailidhk11 <ailidhkinney@gmail.com>
2022-07-20 21:07:24 +0100

    Initial Commit

---
+++ spinev4/spinev4App.swift
@@ -0,0 +1,17 @@
+//
+//  spinev4App.swift
+//  spinev4
+//
+//  Created by Ailidh Kinney on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct spinev4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
